23b65c7747dacc0aff4044a832d7421432201e15
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-06-12 17:02:35 -0400

    comsection changehand

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -3,22 +3,32 @@ import React from 'react'
 
 
 class CommentSection extends React.Component {
+    state = {
+        comments: this.props.comments,
+        newComment: ''
 
+    }
+
+    changeHandler = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
 
     render() {
         return (
             <div className="comment-section">
-                {this.props.comments.map((comment) => {
-                    return (
-                        <div className="comment">
+             {this.state.comments.map((comment) => {
+             return (
+                <div className="comment">
 
-                        <h3>{comment.username}</h3>
+                <h3>{comment.username}</h3>
                         
-                            <p>{comment.text}</p>
+                <p>{comment.text}</p>
 
-                            <p className="timestamp">{this.props.timestamp}</p>
-                            
-                            <input type="text" placeholder="Add a Comment.." />
+                <p className="timestamp">{this.props.timestamp}</p>
+
+                <input type="text" name='newComment' value={this.state.newComment}  placeholder="Add a Comment.." />
                             
                         </div>
                     );
